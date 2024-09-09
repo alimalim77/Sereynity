a37154ea84242628f8d900824e4fb709512ef631
Alim <alimalim77@gmail.com>
2024-09-09 12:17:23 +0530

    Branch Setup for writing integration tests

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,81 +1,80 @@
-const userService = require('../services/auth.service')
-const { sendEmail } = require('../utils/nodemail.util')
-const { generateAuthenticationToken } = require('../services/token.service')
+const userService = require("../services/auth.service");
+const { sendEmail } = require("../utils/nodemail.util");
+const { generateAuthenticationToken } = require("../services/token.service");
 
 const postRegister = async (req, res) => {
   try {
-    console.log('Email code ran successfully')
-    const { email, password } = req.body
+    const { email, password } = req.body;
     const user = await userService.registerUser({
       email: email,
-      password: password
-    })
-    const { otp, expiresAt } = await sendEmail(email)
-    user.otp = otp
-    user.otpExpiresAt = expiresAt
-    user.save()
-    res.status(201).send({ message: 'User registered successfully', user })
+      password: password,
+    });
+    const { otp, expiresAt } = await sendEmail(email);
+    user.otp = otp;
+    user.otpExpiresAt = expiresAt;
+    user.save();
+    res.status(201).send({ message: "User registered successfully", user });
   } catch (error) {
-    if (error.message.includes('Duplicate Email')) {
-      return res.status(409).send({ message: error.message })
+    if (error.message.includes("Duplicate Email")) {
+      return res.status(409).send({ message: error.message });
     }
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
   }
-}
+};
 
 // Need to set email in localStorage to receive it here in body
 // Check JWT expiration time with expiry time of token
 const verifyRegister = async (req, res) => {
   try {
-    const { email, otp } = req.body
+    const { email, otp } = req.body;
 
-    const user = await userService.verifyUser(email)
+    const user = await userService.verifyUser(email);
     if (!user || !user._id) {
-      return res.status(400).json({ message: 'Invalid request' })
+      return res.status(400).json({ message: "Invalid request" });
     }
 
-    const isOtpInvalid = user.otp !== otp
-    const isOtpExpired = user.otpExpiresAt.getTime() < Date.now()
+    const isOtpInvalid = user.otp !== otp;
+    const isOtpExpired = user.otpExpiresAt.getTime() < Date.now();
     if (isOtpInvalid || isOtpExpired) {
-      return res.status(401).json({ message: 'Invalid or expired OTP' })
+      return res.status(401).json({ message: "Invalid or expired OTP" });
     }
 
-    verifyUser(user)
-    const token = await generateAuthenticationToken(user)
+    verifyUser(user);
+    const token = await generateAuthenticationToken(user);
 
-    return res.status(201).json({ user, token })
+    return res.status(201).json({ user, token });
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
-}
+};
 
 const postLogin = async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = await userService.loginUser({ email, password })
+    const user = await userService.loginUser({ email, password });
 
     if (!user) {
       return res
         .status(401)
-        .json({ message: 'Unauthorized or Invalid Credentials' })
+        .json({ message: "Unauthorized or Invalid Credentials" });
     }
 
-    return res.status(200).json(user)
+    return res.status(200).json(user);
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ message: error.message });
   }
-}
+};
 
-const verifyUser = user => {
-  user.isVerified = true
-  user.otp = undefined
-  user.otpExpiresAt = undefined
-  user.save()
-}
+const verifyUser = (user) => {
+  user.isVerified = true;
+  user.otp = undefined;
+  user.otpExpiresAt = undefined;
+  user.save();
+};
 
 module.exports = {
   postRegister,
   postLogin,
-  verifyRegister
-}
+  verifyRegister,
+};
--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,47 +1,46 @@
-const { User } = require('../models/user.model')
-const bcrypt = require('bcryptjs')
+const { User } = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async ({ email, password }) => {
   try {
-    const user = new User({ email, password })
-    await user.save()
-    return user
+    const user = new User({ email, password });
+    await user.save();
+    return user;
   } catch (error) {
     if (error.code === 11000) {
       // Custom error for duplicate email
       throw new Error(
-        'Duplicate Email: The email address is already registered.'
-      )
+        "Duplicate Email: The email address is already registered."
+      );
     } else {
       // General error
-      throw new Error(`Couldn't register user: ${error.message}`)
+      throw new Error(`Couldn't register user: ${error.message}`);
     }
   }
-}
+};
 
 const loginUser = async ({ email, password }) => {
   try {
-    const response = await User.findOne({ email: email })
+    const response = await User.findOne({ email: email });
 
-    const checkPassword = await bcrypt.compare(password, response.password)
-    return checkPassword ? response : undefined
+    const checkPassword = await bcrypt.compare(password, response.password);
+    return checkPassword ? response : undefined;
   } catch (error) {
-    console.log(error)
-    throw new Error("Couldn't find user", error)
+    throw new Error("Couldn't find user", error);
   }
-}
+};
 
-const verifyUser = async email => {
+const verifyUser = async (email) => {
   try {
-    const response = await User.findOne({ email })
-    return response
+    const response = await User.findOne({ email });
+    return response;
   } catch (error) {
-    throw new Error("Couldn't find user", error)
+    throw new Error("Couldn't find user", error);
   }
-}
+};
 
 module.exports = {
   registerUser,
   loginUser,
-  verifyUser
-}
+  verifyUser,
+};
