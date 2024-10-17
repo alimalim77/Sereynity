3f3cb93e6626a01b83ed52a2370c9b3923b4546b
Alim <alimalim77@gmail.com>
2024-10-17 10:36:44 +0530

    Cleanup of meditation function

--- frontend/src/components/Meditation/Meditation.jsx
+++ frontend/src/components/Meditation/Meditation.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
-import { useSelector, useDispatch } from "react-redux";
-import { trigger } from "../../redux/authenticationSlice";
 
 const Meditation = () => {
-  const triggerValue = useSelector((state) => state.authentication.value);
-  const dispatch = useDispatch();
-  const handleClick = () => {
-    console.log("handleClick");
-    dispatch(trigger(triggerValue));
-  };
   return (
     <div>
-      <Button onClick={handleClick}>Hello World</Button>
     </div>
   );
 };
