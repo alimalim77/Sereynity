9cf89264805ad739865066533e77b3dd97055903
Alim <alimalim77@gmail.com>
2024-11-30 22:47:56 +0530

    test: deployment for vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URI,
+  baseURL: "https://sereynity.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
