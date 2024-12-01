8df5cee86fb540e6150be6b1aa64f4705dfd7fe1
Alim <alimalim77@gmail.com>
2024-12-01 12:35:12 +0530

    service: moving from render to vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sereynity.onrender.com",
+  baseURL: "https://sereynity.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
